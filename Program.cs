fff8cc86a54a85590375ac7c3dd8d4912a5f2783
Javier <https://github.com/JavierMarSan18>
2021-06-03 10:50:41 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+			Console.WriteLine("INGRESAR PALABRA");
+			palabra = Console.ReadLine();
+			
+			esPalindromo(palabra);
+			
+			Console.ReadKey();
+        }
+		
+		public static void esPalindromo(string palabra)
+		{
+			palabra = palabra.Replace(" ", "");
+			string palabraInvertida = invertirPalabra(palabra);
+			
+			
+			if(palabra == palabraInvertida)
+			{
+				Console.WriteLine("ES PALINDROMO");
+			}
+			else
+			{
+				Console.WriteLine("NO ES PALINDROMO");
+			}
+		}
+		
+		public static string invertirPalabra(string palabra)
+		{
+			string palabraInvertida = "";
+			
+			foreach(char c in palabra)
+			{
+				palabraInvertida = c + palabraInvertida;
+			}
+			
+			
+			return palabraInvertida;
+		}
+    }
+}
